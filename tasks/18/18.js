4acb44dbf1d350eae1d2ceb2a3004ac2f54a036a
lidasharova <Lida050496@yandex.ru>
2023-10-16 00:28:45 +0300

    feat: complete 18 task

---
+++ tasks/18/18.js
@@ -0,0 +1,37 @@
+/*ЗАДАЧА
+Подсчитать максимальный объем данных, который можно записать в localStorage вашего браузера.
+*/
+//создадим ф-цию в кот подсчитаем размер LocalStorage
+function getSize() {
+  let key = '1';
+  let value = 'value';
+  localStorage.clear(); //очистим localStorage
+  try {
+    //запустим цикл добавления данных в localStorage пока не вылетит ошибка когда он переполнится
+    while (true) {
+      localStorage.setItem(key, value);
+      //меняем значения ключей каждую итерацию, что бы они не повторялись(не перезаписывались)
+      key = key + 1;
+      console.log('Данные успешно добавлены в localStorage');
+    }
+  } catch {
+    console.log('Ошибка при добавлении данных, localStorage переполнен');
+    //распарсим в строку данные из localStorage
+    const string = JSON.stringify(localStorage);
+    //вернем длину строки
+    return string.length;
+  }
+}
+
+document.querySelector('button').addEventListener('click', () => {
+  //получим размер localStorage и выведем на страничку
+  const maxLocalStorageSize = getSize();
+  textInfo = `Размер вашего хранилища: ${maxLocalStorageSize} байт
+  или ${Math.floor(maxLocalStorageSize / 1024 / 1024)} Мб;
+  `;
+  const text = document.createElement('p');
+  text.textContent = textInfo;
+  document.querySelector('p')
+    ? null
+    : document.querySelector('div').append(text);
+});
